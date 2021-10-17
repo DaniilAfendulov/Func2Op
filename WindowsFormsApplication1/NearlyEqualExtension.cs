38b21175bd0e04735a5e05492d5c396b8968f315
Daniil <daniilafendulov@bk.ru>
2021-10-17 19:43:20 +0300

    Add: method for define equality float values with some accuracy

---
+++ WindowsFormsApplication1/NearlyEqualExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncOperationsApplication
+{
+    public static class NearlyEqualExtension
+    {
+        public static bool IsNearlyEqual(this float a, float b, float epsilon = float.Epsilon)
+        {
+            float absA = Math.Abs(a);
+            float absB = Math.Abs(b);
+            float diff = Math.Abs(a - b);
+
+            if (a == b)
+            { // shortcut, handles infinities
+                return true;
+            }
+            else if (a == 0 || b == 0 || absA + absB < float.Epsilon)
+            {
+                // a or b is zero or both are extremely close to it
+                // relative error is less meaningful here
+                return diff < (epsilon * float.Epsilon);
+            }
+            else
+            { // use relative error
+                return diff / (absA + absB) < epsilon;
+            }
+        }
+    }
+}
